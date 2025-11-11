c0eb77c26a8d273f7f37218bf1185353f4427576
ostapin <ostapmisurin3@gmail.com>
2025-11-11 13:50:50 +0300

    Update ui-manager.js

--- js/modules/ui-manager.js
+++ js/modules/ui-manager.js
@@ -1,5 +1,6 @@
-const UIManager = {
-    init() {
-        console.log("UI Manager initialized");
-    }
-};
+function openTab(tabName) {
+    document.querySelectorAll('.tab-content').forEach(tab => tab.classList.remove('active'));
+    document.querySelectorAll('.tab-btn').forEach(btn => btn.classList.remove('active'));
+    document.getElementById(tabName + '-tab').classList.add('active');
+    event.currentTarget.classList.add('active');
+}
