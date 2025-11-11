bb9a8f5351f40f5e9a286c3c4ad4be7888150448
ostapin <ostapmisurin3@gmail.com>
2025-11-11 11:32:01 +0300

    Create spells-data.js

---
+++ js/modules/spells-data.js
@@ -0,0 +1,10 @@
+const spellsBySchool = {
+    "Магия воды": [
+        {
+            name: "Роса",
+            order: 10,
+            effect: "Заклинание покрывает тело тонкой водяной плёнкой",
+            learned: false
+        }
+    ]
+};
