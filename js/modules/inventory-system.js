e027f2b986740e84af997f7547add3bf48066ba6
ostapin <ostapmisurin3@gmail.com>
2025-11-14 18:32:33 +0300

    Delete js/modules directory

--- js/modules/inventory-system.js
+++
@@ -1,3 +0,0 @@
-const InventorySystem = {
-    items: []
-};
--- js/modules/ui-manager.js
+++
@@ -1,6 +0,0 @@
-function openTab(tabName) {
-    document.querySelectorAll('.tab-content').forEach(tab => tab.classList.remove('active'));
-    document.querySelectorAll('.tab-btn').forEach(btn => btn.classList.remove('active'));
-    document.getElementById(tabName + '-tab').classList.add('active');
-    event.currentTarget.classList.add('active');
-}
