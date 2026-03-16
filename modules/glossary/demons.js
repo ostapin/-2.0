967218941a75298833c2f91b20d2ba2f01985fca
ostapin <ostapmisurin3@gmail.com>
2026-03-17 00:13:54 +0300

    Update demons.js

--- modules/glossary/demons.js
+++ modules/glossary/demons.js
@@ -1,1 +1,31 @@
+const demonsData = {
+    general: {
+        description: "Здесь будет общее описание демонов, их происхождение и легенды..."
+    },
+    
+    traits: [
+        "1. Особенность первая...",
+        "2. Особенность вторая...",
+        "3. Особенность третья..."
+    ],
+    
+    groups: {
+        // Здесь будут группы демонов
+        lesser: {
+            name: "Низшие демоны",
+            creatures: {}
+        },
+        greater: {
+            name: "Высшие демоны",
+            creatures: {}
+        },
+        lords: {
+            name: "Повелители демонов",
+            creatures: {}
+        }
+    }
+};
 
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = demonsData;
+}
