e703cd92fa7b8ee76ef3e51930c8b7697fb1e2e7
ostapin <ostapmisurin3@gmail.com>
2026-03-17 16:21:08 +0300

    Update magic.js

--- modules/glossary/magic.js
+++ modules/glossary/magic.js
@@ -1,1 +1,46 @@
-
+// Данные по магии
+const magicData = {
+    spells: {
+        fire: {
+            name: "Огонь",
+            schools: {}
+        },
+        earth: {
+            name: "Земля",
+            schools: {}
+        },
+        water: {
+            name: "Вода",
+            schools: {
+                moisture: {
+                    name: "Влага",
+                    level: 1,
+                    type: "Точечное заклятие",
+                    effect: "Создает тонкую водяную пленку на теле и одежде, предотвращая иссушение организма в пустыне или океане. Альтернативно, генерирует влагу из ничего на поверхности в указанной точке.",
+                    cast_time: "1 магический жест (1 секунда)",
+                    duration: "1 час",
+                    cooldown: "Можно применять повторно сразу после рассеивания",
+                    cost: "30 маны",
+                    enhancement: "При усилении заклинания увеличивается время действия и уровень охлаждения."
+                }
+            }
+        },
+        // остальные школы пока пустые
+        air: { name: "Воздух", schools: {} },
+        metal: { name: "Металл", schools: {} },
+        nature: { name: "Природа", schools: {} },
+        light: { name: "Свет", schools: {} },
+        dark: { name: "Тьма", schools: {} },
+        inferno: { name: "Инферно", schools: {} },
+        chaos: { name: "Хаос", schools: {} },
+        mind: { name: "Разум", schools: {} },
+        life: { name: "Жизнь", schools: {} },
+        death: { name: "Смерть", schools: {} },
+        void: { name: "Пустота", schools: {} },
+        blood: { name: "Кровь", schools: {} },
+        energy: { name: "Энергия", schools: {} },
+        ether: { name: "Эфир", schools: {} }
+    },
+    formation: {},
+    runes: {}
+};
