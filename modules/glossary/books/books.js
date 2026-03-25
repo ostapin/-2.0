d25083601442a5b0b47d258884f6876cb6700058
ostapin <ostapmisurin3@gmail.com>
2026-03-25 17:44:35 +0300

    Update books.js

--- modules/glossary/books/books.js
+++ modules/glossary/books/books.js
@@ -1,1 +1,23 @@
-
+const booksData = [
+    {
+        id: "alchemist_vol1",
+        title: "Приключения алхимика. Том 1",
+        author: "Мартин Лортекс",
+        description: "Первая книга о странствиях алхимика...",
+        file: "modules/glossary/books/alchemist_vol1.html"
+    },
+    {
+        id: "alchemist_vol2",
+        title: "Приключения алхимика. Том 2",
+        author: "Мартин Лортекс",
+        description: "Продолжение истории...",
+        file: "modules/glossary/books/alchemist_vol2.html"
+    },
+    {
+        id: "alchemist_vol3",
+        title: "Приключения алхимика. Том 3",
+        author: "Мартин Лортекс",
+        description: "Завершающая часть...",
+        file: "modules/glossary/books/alchemist_vol3.html"
+    }
+];
