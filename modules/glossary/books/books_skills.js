ccbc6669aa559c39c492c20fabe568b1a3dda965
ostapin <ostapmisurin3@gmail.com>
2026-04-23 09:51:11 +0300

    Update books_skills.js

--- modules/glossary/books/books_skills.js
+++ modules/glossary/books/books_skills.js
@@ -1,1 +1,37 @@
-
+const booksDataSkills = [
+    {
+        id: "survival_forest_vol1",
+        title: "Выживание в лесу. Том 1",
+        author: "",
+        description: "Выживание в лесу. Том 1.",
+        file: ""
+    },
+    {
+        id: "survival_forest_vol2",
+        title: "Выживание в лесу. Том 2",
+        author: "",
+        description: "Выживание в лесу. Том 2.",
+        file: ""
+    },
+    {
+        id: "survival_mountains_vol1",
+        title: "Выживание в горах. Том 1",
+        author: "",
+        description: "Выживание в горах. Том 1.",
+        file: ""
+    },
+    {
+        id: "hunting_vol1",
+        title: "Охота. Том 1",
+        author: "",
+        description: "Охота. Том 1.",
+        file: ""
+    },
+    {
+        id: "tracking_vol1",
+        title: "Следопытство. Том 1",
+        author: "",
+        description: "Следопытство. Том 1.",
+        file: ""
+    }
+];
