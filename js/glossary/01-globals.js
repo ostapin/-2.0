c620a103208122fc7c4d268213bd0bbb0194447b
ostapin <ostapmisurin3@gmail.com>
2026-05-05 14:47:34 +0300

    Update 01-globals.js

--- js/glossary/01-globals.js
+++ js/glossary/01-globals.js
@@ -1,1 +1,12 @@
+// 01-globals.js - Глобальные переменные глоссария
+let allMetals = [];
+let allWeapons = [];
+let allArmor = [];
+let allCreatures = [];
+let allItems = [];
+let currencyRates = {};
+let allCurrencies = [];
 
+let currentCategory = '';
+let currentSubcategory = '';
+let currentSchool = null;
